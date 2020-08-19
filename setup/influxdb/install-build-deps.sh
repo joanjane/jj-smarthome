59a15226599328a96bf0d993407005bbd1b2ebcc
Joan Jané <jjaneballester@gmail.com>
2020-08-19 02:05:02 +0200

    Fix influx build

--- setup/influxdb/install-build-deps.sh
+++ setup/influxdb/install-build-deps.sh
@@ -5,10 +5,10 @@ apt-get update && apt-get install git curl nano build-essential bzr protobuf-com
 mkdir -p /usr/local/rustup
 mkdir -p /usr/local/cargo
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y --no-modify-path --profile minimal
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y --profile minimal
 
 chmod -R a+w $RUSTUP_HOME $CARGO_HOME
 
-rustup --version
-cargo --version
-rustc --version
+# rustup --version
+# cargo --version
+# rustc --version
