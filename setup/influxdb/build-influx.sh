46678a392bb70adcc90f97fbe29b859391fc1199
Joan Jané <--local>
2020-08-20 17:18:32 +0200

    Fix influx build

--- setup/influxdb/build-influx.sh
+++ setup/influxdb/build-influx.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+set -e
+
+# modify yarn timeout
+yarn config set network-timeout 600000 -g
 
 git clone --depth 1 --branch "v$InfluxDB__Version" https://github.com/influxdata/influxdb.git
 cd influxdb
 
-make
+# remove cypress to make it work
+cd ui
+yarn remove cypress
+cd ..
+
+#make
+./env go build ./cmd/influxd
 
 ls -la
 ls -la bin
 
-cp ./bin/influx /usr/local/bin/
-cp ./bin/influxd /usr/local/bin/
+#cp ./bin/influx /usr/local/bin/
+#cp ./bin/influxd /usr/local/bin/
