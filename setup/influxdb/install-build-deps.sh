66c7af4c07114e2fe30fcfd4a73ead2b7933aedf
Joan Jané <jjaneballester@gmail.com>
2020-08-22 14:08:06 +0200

    Fix influx build

--- setup/influxdb/install-build-deps.sh
+++ setup/influxdb/install-build-deps.sh
@@ -3,15 +3,7 @@ set -e
 
 apt-get update && apt-get install git curl nano build-essential bzr protobuf-compiler libprotobuf-dev -y
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-
-curl -sL https://deb.nodesource.com/setup_lts.x | bash -
-apt update && apt-get install -y nodejs yarn
-
-apt remove cmdtest
-
-# install rust
+##############        install rust      #############
 # export RUSTUP_HOME=/usr/local/rustup
 # export CARGO_HOME=/usr/local/cargo
 
@@ -30,6 +22,17 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y --prof
 # export PATH=/usr/local/cargo/bin:$PATH
 source $HOME/.cargo/env
 
-# install go
+rustup --version && cargo --version && rustc --version
+
+##############         install go       #############
 # https://dl.google.com/go/go1.13.15.linux-arm64.tar.gz | tar -C /usr/local -xzf
 # export PATH=$PATH:/usr/local/go/bin
+
+##############      install node+yarn   #############
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+curl -sL https://deb.nodesource.com/setup_lts.x | bash -
+apt update && apt-get install -y nodejs yarn
+
+apt remove cmdtest
