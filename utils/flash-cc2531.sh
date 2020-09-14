a7ad866876f747f3c90f0d235b89652d52b1bcc1
Joan Jané <jjaneballester@gmail.com>
2020-09-15 00:05:50 +0200

    Add script to flash cc2531 stick with raspberry pi

---
+++ utils/flash-cc2531.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+function cleanUp() {
+  rm -rf ./flash_cc2531
+  rm -rf ./firmware
+  rm -rf master.zip
+  rm -rf ./bin
+}
+
+cleanUp
+
+git clone --depth 1 https://github.com/jmichault/flash_cc2531.git
+
+
+chipId=`./flash_cc2531/cc_chipid`
+if [ -z "$(echo $chipId | grep 'b524')" ];
+then
+echo "wrong chip: $chipId. Check your wiring and run script again.";
+exit 1
+fi;
+
+
+rm -rf ./firmware
+
+mkdir firmware
+wget https://github.com/Koenkk/Z-Stack-firmware/archive/master.zip
+unzip master.zip -d ./firmware
+rm master.zip
+
+mkdir bin
+unzip ./firmware/Z-Stack-firmware-master/coordinator/Z-Stack_Home_1.2/bin/default/CC2531_DEFAULT_20190608.zip -d ./bin
+
+
+read -p $'Erase CC2531 dongle? [y/n]\n' confirm
+
+if [ "$confirm" != "y" ];
+then
+echo "Exiting..."
+exit 2
+fi
+
+./flash_cc2531/cc_erase
+
+read -p $'Flash CC2531 dongle? [y/n]\n' confirm
+
+if [ "$confirm" != "y" ];
+then
+echo "Exiting..."
+exit 3
+fi
+
+
+./flash_cc2531/cc_write ./bin/CC2531ZNP-Prod.hex
+
+cleanUp
+
+echo "Done!"
