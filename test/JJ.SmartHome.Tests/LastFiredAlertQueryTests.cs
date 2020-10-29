c72ecf2cdcbfc3fdf0f772a5709ebe655901c2e4
Joan Jané <jjaneballester@gmail.com>
2020-10-30 00:24:17 +0100

    Add integration tests on LastFiredAlertQuery

---
+++ test/JJ.SmartHome.Tests/LastFiredAlertQueryTests.cs
@@ -0,0 +1,59 @@
+﻿using JJ.SmartHome.Core.Alerts.Queries;
+using JJ.SmartHome.Db;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JJ.SmartHome.Tests
+{
+    public class LastFiredAlertQueryTests
+    {
+        [Fact]
+        public async Task GivenAPreviousAlert_WhenGettingLastFiredAlert_ThenReturnDate()
+        {
+            var options = BuildOptions();
+            var influxManagement = new InfluxManagement(options);
+            var authToken = await influxManagement.Setup();
+            options = BuildOptions(authToken);
+
+            var alertsStore = BuildAlertsStore(options);
+            var sut = new LastFiredAlertQuery(alertsStore);
+
+            var date = DateTimeOffset.UtcNow.AddMinutes(-1);
+            const string location = "test";
+
+            await alertsStore.WriteMeasure(new Db.Entities.AlertMeasure
+            {
+                Location = location,
+                Value = 1,
+                Time = date
+            });
+
+            var lastFiredAlertDate = await sut.CheckLastFiredAlertDate();
+
+            Assert.NotNull(lastFiredAlertDate);
+            Assert.Equal(date, lastFiredAlertDate);
+        }
+
+        private static AlertsStore BuildAlertsStore(IOptions<InfluxDbOptions> options)
+        {
+            var influxDBClientProvider = new InfluxDBClientProvider(options);
+            var alertsStore = new AlertsStore(options, influxDBClientProvider.Get());
+            return alertsStore;
+        }
+
+        private static IOptions<InfluxDbOptions> BuildOptions(string token = null)
+        {
+            var configuration = Utils.ConfigBuilder.Build();
+
+            var config = configuration.GetSection("InfluxDB").Get<InfluxDbOptions>();
+            if (!string.IsNullOrEmpty(token))
+            {
+                config.Token = token;
+            }
+            return Options.Create(config);
+        }
+    }
+}
