e221aa69184d5c8f9ae8a4a63293ae86c4eac435
Joan Jané <jjaneballester@gmail.com>
2020-11-06 16:03:13 +0100

    Add sample scaffolding on app

---
+++ src/JJSmartHomeApp/config/AppConfig.ts
@@ -0,0 +1,8 @@
+const config = {
+  appName: "PushDemo",
+  env: "production",
+  apiUrl: "http://localhost:5010",
+  apiKey: "<api-auth-key>",
+};
+
+export default config;
---
+++ src/JJSmartHomeApp/template.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  placeholderName: 'JJSmartHomeApp',
+  templateDir: './template',
+}
