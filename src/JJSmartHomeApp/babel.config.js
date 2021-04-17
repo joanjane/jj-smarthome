07280feba53026b467280e25a390d3dcabba46f2
Joan Jané <jjaneballester@gmail.com>
2021-04-18 01:28:06 +0200

    Fix RN setup

--- src/JJSmartHomeApp/babel.config.js
+++ src/JJSmartHomeApp/babel.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    'module-resolver',
-    {
-      root: ['./'],
-      extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-      alias: {
-        "@core": "./app/core",
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          "@core": "./app/core",
+        }
       }
-    }
+    ]
   ]
 };
